949b23a263be6f70d097b050584f3496265cb5d6
Mario Gutierrez <gmmalberto11@gmail.com>
2023-08-02 16:55:36 -0400

    añadir funcion aws

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const https = require('https');
-const fs = require('fs');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json({ limit: '50mb' }));  
+app.use(bodyParser.json({ limit: '50mb' }));
 
-const uploadFolder = ".";  
+// Crea un objeto S3
+const s3 = new S3Client({ region: 'us-east-1' });
+const bucketName = "cyclic-long-rose-xerus-hose-us-east-1";
 
 app.post('/upload', (req, res) => {
-    const bodyData = req.body.data;  // Recibe los datos a enviar en la solicitud
+    const bodyData = req.body.data;
     const fileName = req.body.file_name;
-    
-    // Configuración de la solicitud
+
     const options = {
         hostname: 'app.useanvil.com',
         port: 443,
         path: '/api/v1/fill/aN0QkXcSWl4GCHI7iVME.pdf',
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json', 
-            'Authorization': 'Basic UWRrWDF6cVNPOEVkaWtnQzNHSmRxMHlsTmc3RTZWQXI6', 
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic UWRrWDF6cVNPOEVkaWtnQzNHSmRxMHlsTmc3RTZWQXI6',
             'Cookie': 'sesh=897784e1a463bcaa47fc71ae02e2482b; sesh.sig=txTRENaY37wogpOStkLDBFCntvE; b_id=1f9fc3360f0b38b5c835433d73d9eeaf; b_id.sig=Em0AnYzHiJx9i7b4VKMnb49Am5c'
         }
     };
 
-    // Realiza la solicitud
     const request = https.request(options, response => {
         let data = [];
 
@@ -34,19 +34,24 @@ app.post('/upload', (req, res) => {
             data.push(chunk);
         });
 
-        response.on('end', () => {
-            // Concatena los chunks de datos y los guarda como un archivo
+        response.on('end', async () => {
             const buffer = Buffer.concat(data);
-            const filePath = path.join(uploadFolder, fileName);
-            fs.writeFile(filePath, buffer, err => {
-                if (err) {
-                    console.log(err);
-                    res.status(500).send("Error al guardar el archivo");
-                } else {
-                    // Retorna la URL del archivo PDF para poder visualizarlo
-                    res.send(req.protocol + '://' + req.get('host') + '/' + fileName);
-                }
-            });
+
+            // Parámetros para guardar en S3
+            const params = {
+                Bucket: bucketName,
+                Key: fileName,
+                Body: buffer
+            };
+
+            // Guardar en S3 usando la versión 3 del SDK
+            try {
+                await s3.send(new PutObjectCommand(params));
+                res.send(`https://${bucketName}.s3.amazonaws.com/${fileName}`);
+            } catch (err) {
+                console.log(err);
+                res.status(500).send("Error al guardar el archivo en S3");
+            }
         });
     });
 
@@ -55,13 +60,8 @@ app.post('/upload', (req, res) => {
         res.status(500).send("Error al hacer la solicitud");
     });
 
-    // Escribe los datos en el cuerpo de la solicitud
     request.write(JSON.stringify(bodyData));
     request.end();
-});
-
-app.get('/:filename', (req, res) => {
-    res.sendFile(path.join(uploadFolder, req.params.filename));
 });
 
 app.listen(5000, '0.0.0.0', () => console.log("Server running on port 5000"));
