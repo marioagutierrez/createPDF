832eec4972ed571118e2470487da48bcbac368b0
Mario Gutierrez <gmmalberto11@gmail.com>
2023-08-05 19:18:51 -0400

    Añadiendo rutas al 009

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const form01Routes = require('./routes/form01');
 const form02Routes = require('./routes/form02');
+const form02Routes = require('./routes/form03');
+const form02Routes = require('./routes/form04');
+const form02Routes = require('./routes/form06');
+const form02Routes = require('./routes/form07');
+const form02Routes = require('./routes/form08');
+const form02Routes = require('./routes/form09');
 
 const app = express();
 
@@ -9,5 +15,11 @@ app.use(express.json({ limit: '50mb' }));
 // Usar las rutas definidas en form01Routes.js
 app.use('/form01', form01Routes);
 app.use('/form02', form02Routes);
+app.use('/form03', form02Routes);
+app.use('/form04', form02Routes);
+app.use('/form06', form02Routes);
+app.use('/form07', form02Routes);
+app.use('/form08', form02Routes);
+app.use('/form09', form02Routes);
 
-app.listen(5000, '0.0.0.0', () => console.log("Server running on port 5000"));
+app.listen(8080, '0.0.0.0', () => console.log("Server running on port 8080"));
---
+++ routes/form09.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const https = require('https');
+const { createClient } = require('@supabase/supabase-js');
+
+const router = express.Router();
+
+// Estas credenciales deben ser almacenadas de forma segura, preferentemente en variables de entorno
+const supabaseUrl = 'https://fpvxrsabicqnqdswijck.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwdnhyc2FiaWNxbnFkc3dpamNrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3MTI4Njk2NywiZXhwIjoxOTg2ODYyOTY3fQ.q8Ekn0o6vCkQcUvNIvVK92VlvPxoXXhwXlN0RJb4YZY';
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  persistSession: false
+});
+
+router.post('/upload', async (req, res) => {
+    const bodyData = req.body.data;
+    const fileName = req.body.file_name;
+
+    const options = {
+        hostname: 'app.useanvil.com',
+        port: 443,
+        path: '/api/v1/fill/unl12lobZJc3X0pZnWR8.pdf',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic UWRrWDF6cVNPOEVkaWtnQzNHSmRxMHlsTmc3RTZWQXI6',
+          'Cookie': 'sesh=897784e1a463bcaa47fc71ae02e2482b; sesh.sig=txTRENaY37wogpOStkLDBFCntvE; b_id=1f9fc3360f0b38b5c835433d73d9eeaf; b_id.sig=Em0AnYzHiJx9i7b4VKMnb49Am5c',
+            },
+        };
+    const request = https.request(options, response => {
+        let data = [];
+
+        response.on('data', chunk => {
+            data.push(chunk);
+        });
+
+        response.on('end', async () => {
+            const buffer = Buffer.concat(data);
+            const { error: uploadError } = await supabase.storage.from('pdfs').upload(fileName, buffer);
+
+            if (uploadError) {
+                console.error(uploadError);
+                return res.status(500).send("Error al subir el archivo");
+            }
+
+            const url = await supabase.storage.from('pdfs').getPublicUrl(fileName);
+            res.json({
+                link: url,
+                file_name: fileName
+            });
+        });
+    });
+
+    request.on('error', error => {
+        console.error(error);
+        res.status(500).send("Error al hacer la solicitud");
+    });
+
+    request.write(JSON.stringify(bodyData));
+    request.end();
+});
+
+module.exports = router;
